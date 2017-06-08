1dad945b0c6feb4a7751f131661a4ec5e37be812
hujimiya <hujimiya@outlook.com>
2017-06-08 23:08:51 +0800

    AC;递归gcd算法

--- src/ALDS1_1_B/Main.java
+++ src/ALDS1_1_B/Main.java
@@ -12,17 +12,15 @@ import java.util.Scanner;
 public class Main {
     static  Scanner scanner = new Scanner(System.in);
     static int gcd(int x,int y){
-        if(y==0){
-            return x;
-        }
-        if(x>y){
-            return gcd(y,x%y);
-        }
-        if(y>x){
-            return gcd(y, x);
-        }
+       if(x==y){
+           return x;
+       }
+       if(y==0){
+           return x;
+       }
 
-        return -1;
+
+        return gcd(y,x%y);
     }
     public static void main(String[] args) {
         int a = scanner.nextInt();
@@ -31,3 +29,19 @@ public class Main {
 
     }
 }
+/*
+ステータス
+Judge: 10/10	JAVA	CPU: 00.06 sec	Memory: 26228 KB	Length: 568 B	2017-06-09 00:07	2017-06-09 00:07
+テストケースの判定結果
+Case #	Verdict	CPU Time	Memory	In	Out	Case Name
+Case #1:	: Accepted	00.05 sec	25968 KB	6 B	2 B	test_case_1
+Case #2:	: Accepted	00.05 sec	26092 KB	9 B	3 B	test_case_2
+Case #3:	: Accepted	00.06 sec	25924 KB	6 B	3 B	test_case_3
+Case #4:	: Accepted	00.05 sec	25972 KB	8 B	2 B	test_case_4
+Case #5:	: Accepted	00.05 sec	26004 KB	12 B	3 B	test_case_5
+Case #6:	: Accepted	00.05 sec	26228 KB	11 B	3 B	test_case_6
+Case #7:	: Accepted	00.05 sec	25936 KB	14 B	3 B	test_case_7
+Case #8:	: Accepted	00.05 sec	26036 KB	18 B	5 B	test_case_8
+Case #9:	: Accepted	00.05 sec	25940 KB	21 B	9 B	test_case_9
+Case #10:	: Accepted	00.05 sec	26000 KB	20 B	2 B	test_case_10
+ */
